a721def0e32e8dbb3666cbc6aa2c1688a715d446
Joseph Sagwe <josephsagwe421@gmail.com>
2023-09-05 09:26:07 +0300

    Update MainActivity.java

--- app/src/main/java/com/example/jsgpt/MainActivity.java
+++ app/src/main/java/com/example/jsgpt/MainActivity.java
@@ -27,9 +27,6 @@ import okhttp3.Request;
 import okhttp3.RequestBody;
 import okhttp3.Response;
 
-
-
-
 public class MainActivity extends AppCompatActivity {
     RecyclerView recyclerView;
     TextView welcomeTextView;
